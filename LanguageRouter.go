b25561afdc5082ad73564836213a7b6aa0150253
Jean-Loup Adde <jean-loup.adde@orange.fr>
2014-05-12 02:28:37 +0200

    HTML Server without detection

--- LanguageRouter.go
+++ LanguageRouter.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import ("log"
+	"net/http"
+)
+
+// The Root path of your static files
+var ROOT_PATH = "/home/juanwolf/Documents/Devel/juanwolf.fr/"
+
+// Setup here what language you want to use.
+var LANG_DEFAULT_DIR = EN_DIR
+var EN_DIR = "en/"
+var FR_DIR = "fr/"
+var ES_DIR = "es/"
+
+var language_detected string
 
 func main() {
-	fmt.Printf("Hello world!")
+	language_detected = LANG_DEFAULT_DIR
+	http.Handle("/" , http.StripPrefix("/",
+			http.FileServer(http.Dir(ROOT_PATH))))
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
